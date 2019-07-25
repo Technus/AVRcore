352f37f367c4a34890a22017dc3da0f2453dff17
Technus <daniel112092@gmail.com>
2019-07-25 17:10:05 +0200

    Tweak functionality

--- src/com/github/technus/avrClone/registerPackages/IRegisterPackageSync.java
+++ src/com/github/technus/avrClone/registerPackages/IRegisterPackageSync.java
@@ -3,5 +3,5 @@ package com.github.technus.avrClone.registerPackages;
 import com.github.technus.avrClone.AvrCore;
 
 public interface IRegisterPackageSync<T> extends IRegisterPackage {
-    void sync(AvrCore core,int[] oldImage,T obj);
+    void sync(AvrCore core);
 }
