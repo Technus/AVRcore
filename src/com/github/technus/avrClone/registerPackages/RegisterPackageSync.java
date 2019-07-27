15c223c45998e8171a0be40aea59ea5601814bee
Technus <daniel112092@gmail.com>
2019-07-27 13:55:20 +0200

    Generify abstract class

--- src/com/github/technus/avrClone/registerPackages/RegisterPackageSync.java
+++ src/com/github/technus/avrClone/registerPackages/RegisterPackageSync.java
@@ -1,6 +1,6 @@
 package com.github.technus.avrClone.registerPackages;
 
-public abstract class RegisterPackageSync extends RegisterPackage implements IRegisterPackageSync {
+public abstract class RegisterPackageSync<T> extends RegisterPackage implements IRegisterPackageSync<T> {
     protected RegisterPackageSync(int offset, int size) {
         super(offset, size);
     }
