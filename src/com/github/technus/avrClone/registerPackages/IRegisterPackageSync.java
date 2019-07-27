278ec026d73895a3f64f4c4b06096721771570d3
Technus <daniel112092@gmail.com>
2019-07-27 14:00:33 +0200

    split sync

--- src/com/github/technus/avrClone/registerPackages/IRegisterPackageSync.java
+++ src/com/github/technus/avrClone/registerPackages/IRegisterPackageSync.java
@@ -3,5 +3,17 @@ package com.github.technus.avrClone.registerPackages;
 import com.github.technus.avrClone.AvrCore;
 
 public interface IRegisterPackageSync<T> extends IRegisterPackage {
-    void sync(AvrCore core,T objToSync);
+    /**
+     * Sync before cpu cycle
+     * @param core
+     * @param objToSync
+     */
+    void preSync(AvrCore core,T objToSync);
+
+    /**
+     * Sync after cpu cycle
+     * @param core
+     * @param objToSync
+     */
+    void postSync(AvrCore core,T objToSync);
 }
